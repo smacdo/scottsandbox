3ad09aa0af147fd05fe36cec35e34c769c01e042
Scott MacDonald <illynova@gmail.com>
2010-12-21 12:24:18 -0600

    adding unit tests, bug fixes and new functionality to simple string tokenizer

--- workbench/simpletokenizer.cpp
+++ workbench/simpletokenizer.cpp
@@ -32,27 +32,82 @@
 ////////////////////////////////////////////////////////////////////////////
 #ifndef SCOTT_WORKBENCH_SIMPLE_TOKENIZER_H
 #define SCOTT_WORKBENCH_SIMPLE_TOKENIZER_H
-#define SIMPLE_TOKENIZER_VERSION 2
+#define SIMPLE_TOKENIZER_VERSION 3
 
 #include <string>
 
+/**
+ * A very simple string tokenizer that transforms a string into a set
+ * of tokens using built-in rules. Tokens are seperated from each other
+ * by either whitespace or an operator character.
+ *
+ * \w*([A-Za-z0-9_]+)(\w|CONTROL_CHARS)
+ */
 class StringTokenizer
 {
 public:
+    /**
+     * Initialize the string tokenizer with an input string
+     */
     StringTokenizer( const std::string& input );
 
+    /**
+     * Indicates if the tokenizer has more tokens to consume. This will
+     * return false if the tokenizer is in an error state.
+     */
     bool hasNext() const;
 
+    /**
+     * Indicates if the tokenizer is in an error state.
+     */
     bool hasError() const;
 
+    /**
+     * Returns the first character position that caused the last error.
+     * Not valid if the parser is not in an error state.
+     */
     size_t errorPos() const;
 
+    /**
+     * Returns a human readable string that describes the error
+     * Note valid if the parser is not in an error state.
+     */
     std::string errorString() const;
 
+    /**
+     * Returns the next parsed token from the input string. If you
+     * continue to request tokens from the tokenizer when there are none
+     * left, the tokenizer will go into an error state and this method
+     * will return empty strings.
+     */
     std::string nextToken();
 
+    /**
+     * Returns the current parsed token from the input string
+     */
+    std::string currentToken() const;
+
 private:
+    /**
+     * Internal method that contains the parsing logic. It will start
+     * from the last point it left off, and create a new token. This will
+     * set an error state if there are problems with parsing, or if the
+     * user attempts to go beyond the end of the string.
+     */
     void parseNext();
+
+    /**
+     * Internal helper method that allows the tokenizer to raise an
+     * error condition. Once raised, the parser will no longer function
+     * and it will report the error
+     */
+    void raiseError( size_t pos, const std::string& message );
+
+    /**
+     * Helper method used by the constructor. It will remove excess spaces
+     * from both the beginning and the end of the input string.
+     */
+    std::string trimString( const std::string& input ) const;
 
 private:
     size_t m_startpos;
@@ -79,23 +134,34 @@ private:
 #include <cassert>
 #include <cctype>
 
+#include <iostream>
+
 StringTokenizer::StringTokenizer( const std::string& input )
     : m_startpos( 0 ),
-      m_endpos( input.size() ),
+      m_endpos( 0 ),
       m_errorpos( 0 ),
-      m_input( input ),
+      m_input( trimString( input ) ),
       m_lastToken(),
       m_currentToken(),
       m_errorStr(),
       m_hasNextToken( true ),
       m_errorFlag( false )
 {
+    m_endpos = m_input.size();
 }
 
 bool StringTokenizer::hasNext() const
 {
     assert (! (m_hasNextToken && m_errorFlag) );
-    return m_hasNextToken;
+
+    if ( hasError() )
+    {
+        return false;
+    }
+    else
+    {
+        return m_hasNextToken;
+    }
 }
 
 bool StringTokenizer::hasError() const
@@ -105,12 +171,33 @@ bool StringTokenizer::hasError() const
 
 size_t StringTokenizer::errorPos() const
 {
-    return m_errorpos;
+    if ( hasError() )
+    {
+        return m_errorpos;
+    }
+    else
+    {
+        // no error, no valid position
+        return std::string::npos;
+    }
 }
 
 std::string StringTokenizer::errorString() const
 {
-    return m_errorStr;
+    if ( hasError() )
+    {
+        return m_errorStr;
+    }
+    else
+    {
+        // no error!
+        return std::string();
+    }
+}
+
+std::string StringTokenizer::currentToken() const
+{
+    return m_currentToken;
 }
 
 std::string StringTokenizer::nextToken()
@@ -175,10 +262,9 @@ void StringTokenizer::parseNext()
             {
                 break;
             }
-            
-            m_errorpos  = cpos;
-            m_errorStr  = "Encountered unprintable ASCII character";
-            m_errorFlag = true;
+         
+            raiseError( cpos,
+                    "Encountered unprintable ASCII Char while parsing" );
             break;
         }
 
@@ -223,20 +309,32 @@ void StringTokenizer::parseNext()
     }
 
     //
-    // Extract the token from the stream
+    // Ensure we didn't go past the end of the string
     //
     if ( token_start == token_end )
     {
-        // only way to trigger this is an error state
-        assert( m_errorFlag );
-        m_hasNextToken = false;
+        // The parser tried was at the end when it asked to go to the next
+        // token. Whoops!
+        raiseError( token_start,
+                    "Attempted to retrieve token beyond end of string" );
     }
-    else
+
+    //
+    // If there have been no errors, extract the next token
+    //
+    if (! hasError() )
     {
+        assert( token_start < token_end );
+
         m_currentToken = m_input.substr(
                                     token_start,
                                     token_end - token_start
         );
+    }
+    else
+    {
+        // In the case of error, make the string be empty
+        m_currentToken = std::string();
     }
 
     if ( parserCurrentPos >= m_endpos )
@@ -245,10 +343,34 @@ void StringTokenizer::parseNext()
     }
 
     // Update parser position
-    m_startpos = parserCurrentPos;
+    m_startpos = token_end;
 
     // Sanity checks
     assert (! (m_hasNextToken && m_errorFlag) );
+}
+
+void StringTokenizer::raiseError( size_t epos, const std::string& message )
+{
+    m_errorpos     = epos;
+    m_errorStr     = message;
+    m_errorFlag    = true;
+    m_hasNextToken = false;
+}
+
+std::string StringTokenizer::trimString( const std::string& input ) const
+{
+    std::string::size_type left = 0, right = input.size();
+
+    // left trim
+    std::string::size_type a = input.find_first_not_of(" \t\r\n");
+    left = ( a != std::string::npos ? a : input.size() );
+
+    // right trim
+    std::string::size_type b = input.find_last_not_of(" \t\r\n");
+    right = ( b != std::string::npos ? b : 0 );
+
+    // apply trim and return result
+    return std::string( input, left, (right-left+1) );
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -259,4 +381,118 @@ void StringTokenizer::parseNext()
 TEST(StringTokenizer,CreateItAndDoNothingOfUseWhatsoever)
 {
     StringTokenizer st( "hallo" );
+    EXPECT_FALSE( st.hasError() );
 }
+
+TEST(StringTokenizer,TokenizeASingleWord)
+{
+    // init
+    StringTokenizer st( "hallo" );
+    EXPECT_FALSE( st.hasError() );
+    EXPECT_TRUE( st.hasNext()   );
+
+    // first and only token
+    std::string token = st.nextToken();
+    EXPECT_EQ( "hallo", token );
+    EXPECT_FALSE( st.hasError() );
+    EXPECT_FALSE( st.hasNext()  );
+}
+
+TEST(StringTokenizer,TokenizerTokenizesTwoWords)
+{
+    StringTokenizer st( "hello world" );
+    std::string token;
+
+    // first token
+    token = st.nextToken();
+    EXPECT_EQ( "hello", token );
+    EXPECT_TRUE( st.hasNext() );
+    EXPECT_FALSE( st.hasError() );
+
+    // second token
+    token = st.nextToken();
+    EXPECT_EQ( "world", token );
+    EXPECT_FALSE( st.hasNext() );
+    EXPECT_FALSE( st.hasError() );
+}
+
+TEST(StringTokenizer,TokenizerWithSymbol)
+{
+    StringTokenizer st( "1 + 2");
+
+    EXPECT_EQ( "1", st.nextToken() );
+    EXPECT_EQ( "+", st.nextToken() );
+    EXPECT_EQ( "2", st.nextToken() );
+
+    EXPECT_FALSE( st.hasError() );
+    EXPECT_FALSE( st.hasNext()  );
+}
+
+TEST(StringTokenizer,TokenizerWithTwoConsequtiveSymbols)
+{
+    StringTokenizer st("1 += 2");
+
+    EXPECT_EQ( "1", st.nextToken() );
+    EXPECT_EQ( "+", st.nextToken() );
+    EXPECT_EQ( "=", st.nextToken() );
+    EXPECT_EQ( "2", st.nextToken() );
+}
+
+TEST(StringTokenizer,TokenizerWithTwoSymbolsInterleaved)
+{
+    StringTokenizer st("mary = beth . jane");
+
+    EXPECT_EQ( "mary", st.nextToken() );
+    EXPECT_EQ( "=",    st.nextToken() );
+    EXPECT_EQ( "beth", st.nextToken() );
+    EXPECT_EQ( ".",    st.nextToken() );
+    EXPECT_EQ( "jane", st.nextToken() );
+}
+
+TEST(StringTokenizer,TokenizesBasicExpression)
+{
+    StringTokenizer st( "apples = tree.apples:count * 22" );
+
+    EXPECT_EQ( "apples", st.nextToken() );
+    EXPECT_EQ( "=",      st.nextToken() );
+    EXPECT_EQ( "tree",   st.nextToken() );
+    EXPECT_EQ( ".",      st.nextToken() );
+    EXPECT_EQ( "apples", st.nextToken() );
+    EXPECT_EQ( ":",      st.nextToken() );
+    EXPECT_EQ( "count",  st.nextToken() );
+    EXPECT_EQ( "*",      st.nextToken() );
+    EXPECT_EQ( "22",     st.nextToken() );
+
+    EXPECT_FALSE( st.hasError() );
+    EXPECT_FALSE( st.hasNext()  );
+}
+
+TEST(StringTokenizer,TokenizerWithEmptyString)
+{
+    StringTokenizer st( "    hello world    ");
+
+    // first token
+    EXPECT_EQ( "hello", st.nextToken() );
+
+    // second token
+    EXPECT_EQ( "world", st.nextToken() );
+
+    // all done?
+    EXPECT_FALSE( st.hasError() );
+    EXPECT_FALSE( st.hasNext()  );
+}
+
+TEST(StringTokenizer,TokenizerGoPastLastTokenTriggersError)
+{
+    StringTokenizer st( "hello world" );
+
+    EXPECT_EQ( "hello", st.nextToken() );
+    EXPECT_EQ( "world", st.nextToken() );
+    EXPECT_FALSE( st.hasError() );
+
+    EXPECT_EQ( "",      st.nextToken() );
+    EXPECT_TRUE( st.hasError() );
+    EXPECT_FALSE( st.hasNext() );
+    EXPECT_EQ( std::string(), st.currentToken() );
+}
+
