e55985d4fc9d2cfe1a728d27f42ea39e233915eb
Scott MacDonald <illynova@gmail.com>
2011-08-15 21:15:17 -0500

    new doxy template

---
+++ docs/doxytemplates.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file   $FILENAME$
+ * @Author $NAME$ ($EMAIL$)
+ * @date   $CREATE$
+ *
+ * $DESCRIPTION$
+ */
+
+/**
+ * @name    Example API Actions
+ * @brief   Example actions available.
+ * @ingroup example
+ *
+ * This API provides certain actions as an example.
+ *
+ * @param [in] repeat  Number of times to do nothing.
+ *
+ * @retval TRUE   Successfully did nothing.
+ * @retval FALSE  Oops, did something.
+ *
+ * Example Usage:
+ * @code
+ *    example_nada(3); // Do nothing 3 times.
+ * @endcode
+ */
+
+/**
+ * Other tags to consider:
+ *
+ * \todo blahblah
+ * \warning WARNING THIS IS DANGEROUS
+ */
