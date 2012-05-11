7322d0ac76890def4c7932075037f4403c0fcbea
Scott MacDonald <scott@whitespaceconsideredharmful.com>
2012-05-10 20:34:40 -0500

    moving all third party code into the third party directory

--- thirdparty/googletest/testrunner.cpp
+++ thirdparty/googletest/testrunner.cpp
@@ -1,9 +1,9 @@
 #include "googletest/googletest.h"
-#include "app/platform.h"
+#include "common/assert.h"
 
 int main( int argc, char * argv[] )
 {
-    App::setIsInUnitTestMode( true );
+    Debug::setInUnitTestMode( true );
 
     ::testing::InitGoogleTest( &argc, argv );
     return RUN_ALL_TESTS();
