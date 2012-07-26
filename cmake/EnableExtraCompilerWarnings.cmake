c313526716a71628ac9f863f1ba9e29fdc01c91a
Scott MacDonald <scott@whitespaceconsideredharmful.com>
2012-07-25 21:30:07 -0500

    Updating CMake modules. Adding several new additions

--- cmake/EnableExtraCompilerWarnings.cmake
+++ cmake/EnableExtraCompilerWarnings.cmake
@@ -14,6 +14,10 @@
 # Distributed under the Boost Software License, Version 1.0.
 # (See accompanying file LICENSE_1_0.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
+#
+# Modified by: Scott MacDonald <scott@whitespaceconsideredharmful.com>
+#  - Adding more warning options
+#  - Can configure level of warnings wanted
 
 if(__enable_extra_compiler_warnings)
 	return()
@@ -33,26 +37,86 @@ macro(_enable_extra_compiler_warnings_flags)
 		endif()
 	else()
 		include(CheckCXXCompilerFlag)
-		set(_flags)
+        set(_flags "-Wall -Wextra -Werror ")
 
-		check_cxx_compiler_flag(-W SUPPORTS_W_FLAG)
-		if(SUPPORTS_W_FLAG)
-			set(_flags "${_flags} -W")
+        check_cxx_compiler_flag(-Winit-self WARN_INIT_SELF)
+        if(WARN_INIT_SELF)
+			set(_flags "${_flags} -Winit-self")
 		endif()
 
-		check_cxx_compiler_flag(-Wall SUPPORTS_WALL_FLAG)
-		if(SUPPORTS_WALL_FLAG)
-			set(_flags "${_flags} -Wall")
+        check_cxx_compiler_flag(-Wfloat-equal WARN_FLOAT_EQUAL)
+        if(WARN_FLOAT_EQUAL)
+			set(_flags "${_flags} -Wfloat-equal")
 		endif()
 
-		check_cxx_compiler_flag(-Wextra SUPPORTS_WEXTRA_FLAG)
-		if(SUPPORTS_WEXTRA_FLAG)
-			set(_flags "${_flags} -Wextra")
+        check_cxx_compiler_flag(-Wsign-conversion WARN_SIGN_CONVERSION)
+        if(WARN_SIGN_CONVERSION)
+			set(_flags "${_flags} -Wsign-conversion")
 		endif()
+
+        check_cxx_compiler_flag(-Wuninitialized WARN_UNINITIALIZED)
+        if(WARN_UNINITIALIZED)
+			set(_flags "${_flags} -Wuninitialized")
+		endif()
+
+        check_cxx_compiler_flag(-Wpointer-arith WARN_POINTER_ARITH)
+        if(WARN_POINTER_ARITH)
+			set(_flags "${_flags} -Wpointer-arith")
+		endif()
+
+        check_cxx_compiler_flag(-Wconversion WARN_CONVERSION)
+        if(WARN_CONVERSION)
+			set(_flags "${_flags} -Wconversion")
+		endif()
+
+        check_cxx_compiler_flag(-Wdtor-privacy WARN_DTOR_PRIVACY)
+        if(WARN_DTOR_PRIVACY)
+			set(_flags "${_flags} -Wdtor-privacy")
+		endif()
+
+        check_cxx_compiler_flag(-Wnon-virtual-dtor WARN_NON_VIRTUAL_DTOR)
+        if(WARN_NON_VIRTUAL_DTOR)
+			set(_flags "${_flags} -Wnon-virtual-dtor")
+		endif()
+
+        check_cxx_compiler_flag(-Woverloaded-virtual WARN_OVERLOADED_VIRTUAL)
+        if(WARN_OVERLOADED_VIRTUAL)
+			set(_flags "${_flags} -Woverloaded-virtual")
+		endif()
+
+        check_cxx_compiler_flag(-Wlogical-op WARN_LOGICAL_OP)
+        if(WARN_LOGICAL_OP)
+			set(_flags "${_flags} -Wlogical-op")
+		endif()
+
+        check_cxx_compiler_flag(-Wdouble-promotion WARN_DOUBLE_PROMOTION)
+        if(WARN_DOUBLE_PROMOTION)
+			set(_flags "${_flags} -Wdouble-promotion")
+		endif()
+
+        check_cxx_compiler_flag(-Wnarrowing WARN_NARROWING)
+        if(WARN_NARROWING)
+			set(_flags "${_flags} -Wnarrowing")
+		endif()
+
+        check_cxx_compiler_flag(-Wzero-as-null-pointer-constant WARN_NULL_CONSTANT)
+        if(WARN_NULL_CONSTANT)
+			set(_flags "${_flags} -Wzero-as-null-pointer-constant")
+		endif()
+
+#        check_cxx_compiler_flag(-Wundef WARN_UNDEF)
+#        if(WARN_UNDEF)
+#			set(_flags "${_flags} -Wundef")
+#		endif()
+
+        check_cxx_compiler_flag(-Wcast-qual WARN_CAST_QUAL)
+        if(WARN_CAST_QUAL)
+            set(_flags "${_flags} -Wcast-qual")
+        endif()
 	endif()
 endmacro()
 
-function(enable_extra_compiler_warnings _target)
+function(enable_warnings _target)
 	_enable_extra_compiler_warnings_flags()
 	get_target_property(_origflags ${_target} COMPILE_FLAGS)
 	if(_origflags)
@@ -68,9 +132,24 @@ function(enable_extra_compiler_warnings _target)
 			COMPILE_FLAGS
 			"${_flags}")
 	endif()
-
 endfunction()
 
+function(set_warnings_as_errors _target)
+	get_target_property(_origflags ${_target} COMPILE_FLAGS)
+	if(_origflags)
+		set_property(TARGET
+			${_target}
+			PROPERTY
+			COMPILE_FLAGS
+            "-Werror ${_origflags}")
+	else()
+		set_property(TARGET
+			${_target}
+			PROPERTY
+			COMPILE_FLAGS
+            "-Werror")
+	endif()
+endfunction()
 function(globally_enable_extra_compiler_warnings)
 	_enable_extra_compiler_warnings_flags()
 	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${_flags}" PARENT_SCOPE)
